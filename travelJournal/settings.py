391ad7bece02611f42a9cf145a1a787a69846f1c
KaelorFogo <kfogo19@gmail.com>
2024-02-02 16:01:42 -0700

    Auth complete

--- travelJournal/settings.py
+++ travelJournal/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 # Add this variable to specify where successful logins should redirect to
-LOGIN_REDIRECT_URL = '/home/'
+LOGIN_REDIRECT_URL = '/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
